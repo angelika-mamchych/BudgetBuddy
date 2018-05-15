c04d59d585d82e41589a0294cec8d87af16fa90d
angelika-mamchych <30314063+angelika-mamchych@users.noreply.github.com>
2018-05-15 13:49:17 +0200

    Integrated flask with MySQL and save flows in mysql db (#4)
    * Integrated flask with MySQL and save flows in mysql db
    
    * Data.py not use anymore

--- forms.py
+++ forms.py
@@ -2,7 +2,9 @@ from wtforms import Form, StringField, validators, IntegerField, RadioField
 
 
 class FlowForm(Form):
-    flowname = StringField('Flow Nameeee', validators=[validators.input_required()])
+    flowname = StringField('Flow Name', validators=[validators.input_required()])
+    stepname1 = StringField("Step Name", validators=[validators.input_required()])
     amount1 = IntegerField("Fee", validators=[validators.input_required()])
     #type = RadioField('Type', choices=['Fixed', 'Percent'])
+    stepname2 = StringField("Step Name", validators=[validators.input_required()])
     amount2 = IntegerField("Fee", validators=[validators.input_required()])
