154667a386972f3601cbe9d7b5211633923254d3
Angelika <30314063+angelika-mamchych@users.noreply.github.com>
2018-07-02 16:40:30 +0200

    Added WTForms (#21)
    WTForms and SqlAlchemy
    
    * Some fields converted on efit_flow_form
    * Edit_flow_form refactored
    * Ended created wtforms
    * Create SQLAlchemy
    * Replaced old db code with sqlAlcemy

--- app.py
+++ app.py
@@ -1,98 +1,91 @@
 # -*- coding: utf-8 -*-
-from typing import Any, Union
 
 from flask import Flask, request, render_template, redirect, url_for
-from flask_mysqldb import MySQL
+from forms import FlowForm
+from flask_sqlalchemy import SQLAlchemy
+import models as m
 
 app = Flask(__name__)
 
 app.config.update(dict(
-    MYSQL_USER="root",
-    MYSQL_PASSWORD="mysql2Lika",
-    MYSQL_DB="budgetbuddy",
-    MYSQL_CURSORCLASS="DictCursor",
+    SQLALCHEMY_DATABASE_URI='mysql://root:mysql2Lika@localhost/budgetbuddy',
+    SQLALCHEMY_TRACK_MODIFICATIONS=False,
     DEBUG=True,
 ))
 
-mysql = MySQL(app)
+db = SQLAlchemy(app)
+
 
 @app.route("/")
 def form():
     return render_template("home.html")
 
 
-@app.route("/new_flow")
+@app.route("/new_flow", methods=['GET'])
 def flow_form():
-    return render_template("flow_form.html")
+    form = FlowForm()
+
+    return render_template("flow_form.html", form=form, buttontext='Create Flow')
 
 
-@app.route("/flow_create", methods=['POST'])
+@app.route("/new_flow", methods=['POST'])
 def flow_create():
-    flowname = request.form['flowname'].strip()
-    stepname1 = request.form['stepname1'].strip()
-    stepname2 = request.form['stepname2'].strip()
-    stepname3 = request.form['stepname3'].strip()
-    steptype1 = request.form['steptype1'].strip()
-    steptype2 = request.form['steptype2'].strip()
-    steptype3 = request.form['steptype3'].strip()
-    amount1 = request.form['amount1'].strip()
-    amount2 = request.form['amount2'].strip()
-    amount3 = request.form['amount3'].strip()
+    form = FlowForm(request.form)
 
-    errors = []
-    if not flowname:
-        errors.append('Please put flowname')
+    if not form.validate():
+        return render_template("flow_form.html", form=form, buttontext='Create Flow')
 
-    amount = request.form["amount1"]
-    if not amount:
-        errors.append('Please put amount')
+    flow = m.Flow(
+        flowname=form.flowname.data.strip(),
+        stepname1=form.stepname1.data.strip(),
+        stepname2=form.stepname2.data.strip(),
+        stepname3=form.stepname3.data.strip(),
+        steptype1=form.steptype1.data.strip(),
+        steptype2=form.steptype2.data.strip(),
+        steptype3=form.steptype3.data.strip(),
+        amount1=form.amount1.data,
+        amount2=form.amount2.data,
+        amount3=form.amount3.data
+    )
 
-    if errors:
-        return render_template("flow_form.html", errors=errors)
+    db.session.add(flow)
+    db.session.commit()
 
-    cur = mysql.connection.cursor()
-    cur.execute('INSERT INTO results values(NULL, "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}")'.format(
-        flowname, stepname1, stepname2,  stepname3, steptype1, steptype2, steptype3, amount1, amount2, amount3))
-    mysql.connection.commit()
-    return redirect(url_for('flow_item', flow_id=cur.lastrowid))
+    return redirect(url_for('flow_item', flow_id=flow.id))
 
 
 @app.route("/flows/<int:flow_id>", methods=['GET'])
 def flow_item(flow_id):
-    # get from DB by flow_id
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM results where id={}'.format(flow_id))
-    flow = cur.fetchone()
+    flow = m.Flow.query.filter_by(id=flow_id).first()
     return render_template("flow_item.html", flow=flow)
 
 
 @app.route("/flows/<int:flow_id>", methods=['POST'])
 def flow_item_result(flow_id):
     # get from DB by flow_id
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM results where id={}'.format(flow_id))
-    flow = cur.fetchone()
+    flow = m.Flow.query.filter_by(id=flow_id).first()
     total_amount = float(request.form['totalamount'].strip())
-    if flow['steptype1'].lower() == 'percent':
-        step1_fee = total_amount * flow['amount1'] / 100
+
+    if flow.steptype1.lower() == 'percent':
+        step1_fee = total_amount * flow.amount1 / 100
         total_amount -= step1_fee
     else:
-        step1_fee = flow['amount1']
-        total_amount -= flow['amount1']
+        step1_fee = flow.amount1
+        total_amount -= flow.amount1
 
-    if flow['steptype2'] == 'percent':
-        step2_fee = total_amount * flow['amount2'] / 100
+    if flow.steptype2 == 'percent':
+        step2_fee = total_amount * flow.amount2 / 100
         total_amount -= step2_fee
     else:
-        step2_fee = flow['amount2']
-        total_amount -= flow['amount2']
+        step2_fee = flow.amount2
+        total_amount -= flow.amount2
 
-    if flow['steptype3'] == 'percent':
-        step3_fee = total_amount * flow['amount3'] / 100
+    if flow.steptype3 == 'percent':
+        step3_fee = total_amount * flow.amount3 / 100
         total_amount -= step3_fee
     else:
-        step3_fee = flow['amount3']
-        total_amount -= flow['amount3']
+        step3_fee = flow.amount3
+        total_amount -= flow.amount3
 
     return render_template(
         "flow_item.html",
@@ -106,27 +99,21 @@ def flow_item_result(flow_id):
 
 @app.route("/flows")
 def show_flows():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM results')
-    flows = cur.fetchall()
+    flows = m.Flow.query.all()
     return render_template("show_flows.html", flows=flows)
 
 
 @app.route("/flows_compare", methods=['GET'])
 def flows_compare():
     ids = request.args.getlist('ids')
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM results where id IN ({})'.format(", ".join(ids)))
-    flows = cur.fetchall()
+    flows = m.Flow.query.filter(Flow.id.in_(ids)).all()
     return render_template("flows_compare.html", flows=flows)
 
 
 @app.route("/flows_compare", methods=['POST'])
 def flow_compare_result():
     ids = request.args.getlist('ids')
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM results where id IN ({})'.format(", ".join(ids)))
-    flows = cur.fetchall()
+    flows = m.Flow.query.filter(Flow.id.in_(ids)).all()
     total_amount = float(request.form['totalamount'].strip())
     total_lefts = []
     step_one_fees = []
@@ -134,26 +121,26 @@ def flow_compare_result():
     step_three_fees = []
     for flow in flows:
         fee = 0
-        if flow['steptype1'].lower() == 'percent':
-            step1_fee = total_amount * flow['amount1'] / 100
+        if flow.steptype1.lower() == 'percent':
+            step1_fee = total_amount * flow.amount1 / 100
             fee += step1_fee
         else:
-            step1_fee = flow['amount1']
-            fee += flow['amount1']
+            step1_fee = flow.amount1
+            fee += flow.amount1
 
-        if flow['steptype2'] == 'percent':
-            step2_fee = (total_amount - fee) * flow['amount2'] / 100
+        if flow.steptype2 == 'percent':
+            step2_fee = (total_amount - fee) * flow.amount2 / 100
             fee += step2_fee
         else:
-            step2_fee = flow['amount2']
-            fee += flow['amount2']
+            step2_fee = flow.amount2
+            fee += flow.amount2
 
-        if flow['steptype3'] == 'percent':
-            step3_fee = (total_amount - fee) * flow['amount3'] / 100
+        if flow.steptype3 == 'percent':
+            step3_fee = (total_amount - fee) * flow.amount3 / 100
             fee += step3_fee
         else:
-            step3_fee = flow['amount3']
-            fee += flow['amount3']
+            step3_fee = flow.amount3
+            fee += flow.amount3
         total_left = total_amount - fee
         total_lefts.append(round(total_left, 2))
         step_one_fees.append(round(step1_fee, 2))
@@ -171,39 +158,32 @@ def flow_compare_result():
 
 @app.route('/edit_flow/<int:flow_id>', methods=['GET', 'POST'])
 def edit_flow(flow_id):
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM results where id={}'.format(flow_id))
-    flow = cur.fetchone()
-    if request.method == 'POST':
-        flowname = request.form['flowname'].strip()
-        stepname1 = request.form['stepname1'].strip()
-        stepname2 = request.form['stepname2'].strip()
-        stepname3 = request.form['stepname3'].strip()
-        steptype1 = request.form['steptype1'].strip()
-        steptype2 = request.form['steptype2'].strip()
-        steptype3 = request.form['steptype3'].strip()
-        amount1 = request.form['amount1'].strip()
-        amount2 = request.form['amount2'].strip()
-        amount3 = request.form['amount3'].strip()
-        cur.execute(
-            'UPDATE results SET flowname="{}", stepname1="{}", stepname2="{}", stepname3="{}", steptype1="{}", \
-            steptype2="{}", steptype3="{}", amount1="{}", amount2="{}", amount3="{}" WHERE id={}'.format(
-                flowname, stepname1, stepname2, stepname3, steptype1, steptype2, steptype3, amount1, amount2, amount3,
-                flow_id))
-        mysql.connection.commit()
+    flow = m.Flow.query.filter_by(id=flow_id).first()
+    form = FlowForm(request.form, flow)
+
+    if request.method == 'POST' and form.validate():
+        flow.flowname = form.flowname.data.strip()
+        flow.stepname1 = form.stepname1.data.strip()
+        flow.stepname2 = form.stepname2.data.strip()
+        flow.stepname3 = form.stepname3.data.strip()
+        flow.steptype1 = form.steptype1.data.strip()
+        flow.steptype2 = form.steptype2.data.strip()
+        flow.steptype3 = form.steptype3.data.strip()
+        flow.amount1 = form.amount1.data
+        flow.amount2 = form.amount2.data
+        flow.amount3 = form.amount3.data
+
+        db.session.commit()
+
         return redirect(url_for("flow_item", flow_id=flow_id))
     else:
-        return render_template('edit_flow_form.html', flow=flow)
+        return render_template('flow_form.html', form=form, buttontext="Edit Flow")
 
 
 @app.route('/delete_flow/<int:flow_id>', methods=['POST'])
 def delete_flow(flow_id):
-    cur = mysql.connection.cursor()
-    cur.execute("DELETE FROM results WHERE id={}".format(flow_id))
-    mysql.connection.commit()
-    cur.close()
+    m.Flow.query.filter_by(id=flow_id).delete()
     return redirect(url_for('show_flows'))
-
 
 if __name__ == "__main__":
     app.run()
--- forms.py
+++ forms.py
@@ -1,10 +1,14 @@
-from wtforms import Form, StringField, validators, IntegerField, RadioField
+from wtforms import Form, StringField, validators, DecimalField, RadioField
 
 
 class FlowForm(Form):
     flowname = StringField('Flow Name', validators=[validators.input_required()])
     stepname1 = StringField("Step Name", validators=[validators.input_required()])
-    amount1 = IntegerField("Fee", validators=[validators.input_required()])
-    #type = RadioField('Type', choices=['Fixed', 'Percent'])
+    amount1 = DecimalField("Amount", validators=[validators.input_required()])
+    steptype1 = RadioField('Type', choices=[('fixed', 'Fixed'), ('percent', 'Percent')])
     stepname2 = StringField("Step Name", validators=[validators.input_required()])
-    amount2 = IntegerField("Fee", validators=[validators.input_required()])
+    amount2 = DecimalField("Amount", validators=[validators.input_required()])
+    steptype2 = RadioField('Type', choices=[('fixed', 'Fixed'), ('percent', 'Percent')])
+    stepname3 = StringField("Step Name", validators=[validators.input_required()])
+    amount3 = DecimalField("Amount", validators=[validators.input_required()])
+    steptype3 = RadioField('Type', choices=[('fixed', 'Fixed'), ('percent', 'Percent')])
